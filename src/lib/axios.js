ab4c8e92eaf83de8a102a521fe92c5599dcdc8ae
Andrés Santiago Garcés Pradilla <129919422+SaPradilla@users.noreply.github.com>
2023-11-29 19:08:34 +0000

    api config

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // baseURL: 'https://api-votaciones.onrender.com/api'
 const api = axios.create({
-    baseURL: 'http://localhost:5650/api'
+    baseURL: 'https://backend-votaciones-production.up.railway.app/api'
 })
 
 export default api
