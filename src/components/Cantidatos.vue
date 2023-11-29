c7366da35b01c015932472c8c66067db9f07b399
Andrés Santiago Garcés Pradilla <129919422+SaPradilla@users.noreply.github.com>
2023-11-29 22:33:39 +0000

    dsf

--- src/components/Cantidatos.vue
+++ src/components/Cantidatos.vue
@@ -254,7 +254,7 @@ const modalOpen = ()=>{
                                     <button
                                     type="button"
                                     class=" w-max inline-flex justify-center rounded-md border border-transparent bg-green-500 px-4 py-2 text-sm font-medium text-white hover:bg-green-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                                    @click=" candidatoSeleccionado.nombre ? votarBlanco() :  Votar()"
+                                    @click=" candidatoSeleccionado.nombre ? Votar() : votarBlanco()  "
                                     >
                                     Votar {{ candidatoSeleccionado.nombre  ? 'por ' +candidatoSeleccionado.nombre : 'en Blanco ' }}
                                     </button>
